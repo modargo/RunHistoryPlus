57381547ad6c0bbb8312a2c01cb0bec4dd6eaafe
modargo <modargo@gmail.com>
2022-04-27 10:24:26 -0400

    Remove unused using

--- src/main/java/runhistoryplus/patches/FloorExitPlaytimeRunHistoryPatch.java
+++ src/main/java/runhistoryplus/patches/FloorExitPlaytimeRunHistoryPatch.java
@@ -16,7 +16,6 @@ import javassist.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import runhistoryplus.Config;
-import runhistoryplus.RunHistoryPlus;
 
 import java.lang.reflect.Field;
 import java.text.MessageFormat;
