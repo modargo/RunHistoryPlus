9a4bb633fa21ffd8b11fab48a634e12b2c662280
modargo <modargo@gmail.com>
2022-04-29 10:03:17 -0400

    Log skipped rewards from events

--- src/main/java/runhistoryplus/patches/ShopEventCardRewardsSkippedRunHistoryPatch.java
+++ src/main/java/runhistoryplus/patches/ShopEventCardRewardsSkippedRunHistoryPatch.java
@@ -5,16 +5,18 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.rewards.RewardItem;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.rooms.EventRoom;
 import com.megacrit.cardcrawl.rooms.ShopRoom;
 import com.megacrit.cardcrawl.saveAndContinue.SaveFile;
 
-public class ShopCardRewardsSkippedRunHistoryPatch {
+public class ShopEventCardRewardsSkippedRunHistoryPatch {
     @SpirePatch(clz = AbstractDungeon.class, method = "nextRoomTransition", paramtypez = { SaveFile.class })
-    public static class ShopCardRewardsSkippedAddLogging {
+    public static class ShopEventCardRewardsSkippedAddLogging {
         @SpirePrefixPatch
         public static void shopContentsAddLogging(AbstractDungeon __instance, SaveFile saveFile) {
             boolean isLoadingSave = CardCrawlGame.loadingSave && saveFile != null;
-            if (!isLoadingSave && AbstractDungeon.currMapNode != null && AbstractDungeon.currMapNode.room instanceof ShopRoom) {
+            if (!isLoadingSave && AbstractDungeon.currMapNode != null && isRoomWithPotentialCardRewards(AbstractDungeon.currMapNode.room)) {
                 if (!AbstractDungeon.combatRewardScreen.hasTakenAll) {
                     for (RewardItem r : AbstractDungeon.combatRewardScreen.rewards) {
                         if (r.type == RewardItem.RewardType.CARD) {
@@ -24,5 +26,9 @@ public class ShopCardRewardsSkippedRunHistoryPatch {
                 }
             }
         }
+    }
+
+    private static boolean isRoomWithPotentialCardRewards(AbstractRoom room) {
+        return room instanceof ShopRoom || room instanceof EventRoom;
     }
 }
