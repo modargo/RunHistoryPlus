7f03f77eb71de3dbee794ffb83c213515251f0cd
Nekaida <nekaida@nelaco.de>
2022-07-30 18:52:12 +0200

    Show final value in Card popup for Genetic Algorithm and Ritual Dagger
    - if multiples of the same cards were in the deck, the highest value is
      shown

--- src/main/java/runhistoryplus/patches/ImprovableCardRunHistoryPatch.java
+++ src/main/java/runhistoryplus/patches/ImprovableCardRunHistoryPatch.java
@@ -2,13 +2,17 @@ package runhistoryplus.patches;
 
 import basemod.ReflectionHacks;
 import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.cards.blue.GeneticAlgorithm;
 import com.megacrit.cardcrawl.cards.colorless.RitualDagger;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.metrics.Metrics;
 import com.megacrit.cardcrawl.monsters.MonsterGroup;
+import com.megacrit.cardcrawl.screens.runHistory.RunHistoryScreen;
+import com.megacrit.cardcrawl.screens.runHistory.TinyCard;
 import com.megacrit.cardcrawl.screens.stats.RunData;
 import javassist.CannotCompileException;
 import javassist.NotFoundException;
@@ -16,7 +20,9 @@ import javassist.CtBehavior;
 import javassist.CtClass;
 import javassist.CtField;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -46,15 +52,37 @@ public class ImprovableCardRunHistoryPatch {
             improvableCardsLog = new HashMap<>();
             for (final AbstractCard card: AbstractDungeon.player.masterDeck.group) {
                 if (improvableCards.contains(card.cardID)) {
-                    String metricId = card.getMetricID();
-                    if (!improvableCardsLog.containsKey(metricId)) {
-                        improvableCardsLog.put(metricId, new ArrayList<Integer>());
+                    if (!improvableCardsLog.containsKey(card.name)) {
+                        improvableCardsLog.put(card.name, new ArrayList<>());
                     }
-                    improvableCardsLog.get(metricId).add(card.misc);
+                    improvableCardsLog.get(card.name).add(card.misc);
                 }
             }
             ReflectionHacks.privateMethod(Metrics.class, "addData", Object.class, Object.class)
                     .invoke(__instance, "improvable_cards", improvableCardsLog);
+        }
+    }
+
+    @SpirePatch(clz = RunHistoryScreen.class, method = "update")
+    public static class RunHistoryScreenUpdatePatch {
+        @SpireInsertPatch(locator = Locator.class, localvars = "addMe")
+        public static void updateCardsWithFinalValues(RunHistoryScreen __instance, RunData ___viewedRun, TinyCard addMe) throws NoSuchFieldException, IllegalAccessException {
+            if (improvableCards.contains(addMe.card.cardID)) {
+                Field improvableCardsField = ___viewedRun.getClass().getField("improvable_cards");
+                Map<String, List<Double>> improvableCardsLog = (Map<String, List<Double>>)improvableCardsField.get(___viewedRun);
+                if (improvableCardsLog != null && improvableCardsLog.containsKey(addMe.card.name)) {
+                    Integer value = Collections.max(improvableCardsLog.get(addMe.card.name)).intValue();
+                    addMe.card.baseDamage = value;
+                    addMe.card.baseBlock = value;
+                }
+            }
+        }
+
+        public static class Locator extends SpireInsertLocator {
+            public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+                Matcher matcher = new Matcher.MethodCallMatcher(CardGroup.class, "addToTop");
+                return LineFinder.findInOrder(ctMethodToPatch, new ArrayList<>(), matcher);
+            }
         }
     }
 }
